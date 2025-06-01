159b1907b899b7c1b6385dbb41a0722b91f626cf
Himanshu-coder007 <himanshunagose54@gmail.com>
2025-06-01 19:54:57 +0530

    some bugs fixed

--- src/lib/db.ts
+++ src/lib/db.ts
@@ -1,10 +1,5 @@
 import { MongoClient } from 'mongodb';
 
-declare global {
-  // Allow the global object to have a custom property
-  var _mongoClientPromise: Promise<MongoClient> | undefined;
-}
-
 if (!process.env.MONGODB_URI) {
   throw new Error('Invalid/Missing environment variable: "MONGODB_URI"');
 }
@@ -12,18 +7,12 @@ if (!process.env.MONGODB_URI) {
 const uri = process.env.MONGODB_URI;
 const options = {};
 
-let client;
+let client: MongoClient;
 let clientPromise: Promise<MongoClient>;
 
-if (process.env.NODE_ENV === 'development') {
-  if (!global._mongoClientPromise) {
-    client = new MongoClient(uri, options);
-    global._mongoClientPromise = client.connect();
-  }
-  clientPromise = global._mongoClientPromise;
-} else {
+
   client = new MongoClient(uri, options);
   clientPromise = client.connect();
-}
+
 
 export default clientPromise;
